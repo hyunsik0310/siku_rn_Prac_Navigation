e49105af2459e4a8905fbf4eee581f8ef7259e49
hyunsik <hyunsik0310@gmail.com>
2022-09-10 18:49:37 +0900

    nav 4 commit by Windows

--- src/navigations/Stack.js
+++ src/navigations/Stack.js
@@ -3,16 +3,58 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../screens/Home';
 import List from '../screens/List';
 import Item from '../screens/Item';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Platform } from 'react-native';
 
 const Stack = createStackNavigator();
 
 const StackNavigation = () => {
   return (
     //initialRouteName로 초기화면을 제어
-    <Stack.Navigator initialRouteName='Home'>
+    <Stack.Navigator
+      initialRouteName='Home'
+      screenOptions={{
+        cardStyle: { backgroundColor: '#ffffff' },
+        headerStyle: {
+          height: 110,
+          backgroundColor: '#95a5a6',
+          borderBottomWidth: 5,
+          borderBottomColor: '#34495e',
+        },
+        headerTitleStyle: { color: '#ffffff', fontSize: 24 },
+        headerTitleAlign: 'center',
+        headerTitle: ({ style }) => (
+          <MaterialCommunityIcons name='react' style={style} />
+        ),
+      }}
+    >
       <Stack.Screen name='Home' component={Home}></Stack.Screen>
-      <Stack.Screen name='List' component={List}></Stack.Screen>
-      <Stack.Screen name='Item' component={Item}></Stack.Screen>
+      <Stack.Screen
+        name='List'
+        component={List}
+        options={{
+          headerTitle: 'List Screen',
+          headerBackTitleVisible: true,
+          headerBackTitle: 'Prev',
+          headerTitleStyle: { fontSize: 24 },
+          headerTintColor: '#e74c3c',
+          headerBackImage: ({ tintColor }) => {
+            const style = {
+              marginRight: 5,
+              marginLeft: Platform.OS === 'ios' ? 11 : 0,
+            };
+            return (
+              <MaterialCommunityIcons
+                name='keyboard-backspace'
+                size={30}
+                color={tintColor}
+                style={style}
+              />
+            );
+          },
+        }}
+      ></Stack.Screen>
+      <Stack.Screen name='Detail' component={Item}></Stack.Screen>
     </Stack.Navigator>
   );
 };
